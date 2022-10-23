655c30433090469b70f7e7d41ae7478e3a092e3f
Shaikh Irfan <shaikhji1998@gmail.com>
2022-10-23 17:56:29 +0530

    Changes in stylecss and newly added about/blog page

--- App.js
+++ App.js
@@ -12,3 +12,20 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+// single product details
+var MainImg = document.getElementById("mainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function() {
+    MainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    MainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function() {
+    MainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function() {
+    MainImg.src = smallImg[3].src;
+}
