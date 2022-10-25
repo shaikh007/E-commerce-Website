da82371a05347c29b33fbbcbcf957b20bbe4fc94
Shaikh Irfan <shaikhji1998@gmail.com>
2022-10-25 19:46:16 +0530

    final commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const bar = document.getElementById("bar");
-const close = document.getElementById("close");
 const nav = document.getElementById("nav-bar");
+const close = document.getElementById("close");
 
 if (bar) {
     bar.addEventListener('click', () => {
